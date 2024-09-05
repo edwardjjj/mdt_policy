fix: fix success calculation in eval

run.py
@@ -1,5 +1,6 @@
 from multiprocessing import freeze_support
 from pathlib import Path
+from mdt.rollout.rollout_video import RolloutVideo
 
 import click
 import hydra
@@ -8,29 +9,30 @@ import torch
 import wandb
 
 from mdt.evaluation.multistep_sequences import get_sequences
-from mdt.models.rl.env import make_playtable_env_func
+from mdt.models.rl.env import PlayTableTaskEnv, make_playtable_env_func
 from mdt.models.rl.mdtv_residual import MDTVResidual
 from mdt.models.rl.ppo import PPO
 from mdt.models.rl.vector_env import SubprocVecEnv
 
 
-def eval_policy(policy: MDTVResidual, env, num_episodes) -> None:
+def eval_policy(policy: MDTVResidual, env: PlayTableTaskEnv, num_episodes: int) -> None:
     policy.reset()
-    obs, info = env.reset()
     ep_rewards = np.zeros(6)
     for i in range(num_episodes):
-        current_reward = 0
-        while True:
+        obs, info = env.reset()
+        episode_reward = 0
+        done = False
+        while not done:
             with torch.no_grad():
                 actions, _, _, _, _ = policy(obs)
             obs, reward, done, info = env.step(actions)
-            current_reward += reward
-            if done:
-                ep_rewards[current_reward] += 1
-                break
+            episode_reward += reward
+        ep_rewards[episode_reward] += 1
     result = {}
     for i in range(1, 6):
-        result[f"completed {i} tasks"] = ep_rewards[i:6].sum() / ep_rewards[0:6].sum()
+        result[f"completed {i} tasks"] = np.sum(ep_rewards[i:6]) / np.sum(
+            ep_rewards[0:6]
+        )
     wandb.log(result)
 
 
@@ -46,7 +48,7 @@ def eval(eval_config, env_config, eval_ckpt_path) -> None:
     eval_policy(policy, env, eval_config.num_episodes)
 
 
-def train(config, env_config) -> None:
+def train(config, env_config, eval_episodes) -> None:
     run = wandb.init(
         project="residual_calvin_train", entity="aklab", tags=["mdtv", "d"]
     )
@@ -57,11 +59,11 @@ def train(config, env_config) -> None:
     policy: MDTVResidual = hydra.utils.instantiate(config.policy, _recursive_=False)
     policy.configure_mdtv()
     policy.to(torch.device("cuda"))
-    ppo = hydra.utils.instantiate(config.ppo, policy=policy, env=train_env)
+    ppo: PPO = hydra.utils.instantiate(config.ppo, policy=policy, env=train_env)
     ppo.learn(1_000, log_interval=10)
     train_env.close()
-    eval_env = make_playtable_env_func(env_config, sequences)()
-    eval_policy(policy, eval_env, 500)
+    eval_env:PlayTableTaskEnv = make_playtable_env_func(env_config, sequences)() # type:ignore
+    eval_policy(policy, eval_env, eval_episodes)
     torch.save(
         policy.state_dict(),
         f"/home/edward/projects/mdt_policy/checkpoints/{run.id}.ckpt",
@@ -76,7 +78,10 @@ def train(config, env_config) -> None:
 @click.option(
     "--config_name", default="train_residual_calvin_d", help="config name for training"
 )
-def main(eval, eval_ckpt_path, config_name):
+@click.option(
+    "--eval_episodes", default=500, help="number of eval episodes after training"
+)
+def main(eval, eval_ckpt_path, config_name, eval_episodes):
     if eval:
         if eval_ckpt_path is None:
             raise ValueError("eval_ckpt_path can't be None")
@@ -89,7 +94,7 @@ def main(eval, eval_ckpt_path, config_name):
     if eval:
         eval(config, env_config, eval_ckpt_path)
     else:
-        train(config, env_config)
+        train(config, env_config, eval_episodes)
 
 
 if __name__ == "__main__":